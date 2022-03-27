4d7a0ec9991d0f8a669e3c140c8c3e5754f2607d
Chelsea Farley <chelseafarley91@gmail.com>
2022-03-27 20:12:24 +1300

    SectionList

--- App.js
+++ App.js
@@ -1,12 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, SectionList } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  let [data, setData] = React.useState();
+  let [refreshing, setRefreshing] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(loadData, 2000);
+  });
+
+  let refreshData = () => {
+    setRefreshing(true);
+    setTimeout(loadData, 2000);
+  };
+
+  let loadData = () => {
+    let loadedData = [
+      {
+        title: "Overdue",
+        color: "#FF0000",
+        data: [
+          "English Homework",
+          "Wash Dishes",
+          "Wash Hair"
+        ]
+      },
+      {
+        title: "Todo",
+        color: "#000000",
+        data: [
+          "Buy Laura's Birthday Present",
+          "Exercise",
+          "Grocery Shopping"
+        ]
+      },
+      {
+        title: 'Completed',
+        color: "#00FF00",
+        data: [
+          "Laundry",
+          "Make Bed",
+          "Prepare Dinner"
+        ]
+      }
+    ];
+
+    setData(loadedData);
+    setRefreshing(false);
+  };
+
+  let renderItem = ({item}) => {
+    return <Text>{item}</Text>
+  };
+
+  let renderSectionHeader = ({section}) => {
+    return <Text style={[styles.sectionHeader, { color: section.color }]}>{section.title}</Text>
+  };
+
+  let renderSectionFooter = ({section}) => {
+    return <Text style={styles.sectionFooter}>{section.data.length} Items {section.title}</Text>
+  };
+
+  let SectionSeparator = () => {
+    return <View style={styles.sectionSeparator} />
+  };
+
+  let ItemSeparator = () => {
+    return <View style={styles.itemSeparator} />
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <SectionList 
+        sections={data}
+        style={styles.stretch}
+        onRefresh={refreshData}
+        refreshing={refreshing}
+        keyExtractor={(item, index) => item + index}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
+        renderSectionFooter={renderSectionFooter}
+        SectionSeparatorComponent={SectionSeparator}
+        ItemSeparatorComponent={ItemSeparator}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +93,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
+    margin: 16
   },
+  stretch: {
+    alignSelf: 'stretch'
+  },
+  sectionHeader: {
+    textTransform: 'uppercase',
+    textDecorationLine: 'underline',
+    fontSize: 20,
+    paddingVertical: 4,
+    alignSelf: 'stretch',
+    backgroundColor: '#ffffff'
+  },
+  sectionFooter: {
+    fontStyle: 'italic',
+    paddingVertical: 4
+  },
+  itemSeparator: {
+    height: 4,
+    backgroundColor: '#eeeeee',
+    alignSelf: 'stretch'
+  },
+  sectionSeparator: {
+    height: 4,
+    backgroundColor: '#bebebe',
+    alignSelf: 'stretch'
+  }
 });
